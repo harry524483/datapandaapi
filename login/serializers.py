08716d540775d628922497f4b117ee10fdefe1e1
harry524483 <astroharinder.singh@gmail.com>
2018-03-10 22:45:48 +0530

    removed files

--- login/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Auth
-
-class AuthSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Auth
-        fields = '__all__'
